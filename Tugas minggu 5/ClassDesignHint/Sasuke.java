e7541a1d41ffb4c3f7c2da73b721fb4748fc9168
MaolanaF <95175354+MaolanaF@users.noreply.github.com>
2022-03-13 23:50:54 +0700

    Create Sasuke.java

---
+++ Tugas minggu 5/ClassDesignHint/Sasuke.java
@@ -0,0 +1,14 @@
+package classhint;
+
+/**
+ *
+ * @author Maolana
+ */
+public class Sasuke extends Itachi {
+    String Dojutsu = "Sharingan";
+    
+    void printDojutsu() {
+        super.printDojutsu();
+        System.out.println(this.Dojutsu);
+    }
+}
