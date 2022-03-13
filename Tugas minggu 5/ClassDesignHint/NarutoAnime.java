f68b1a8a3f0ab03bcfca13c67936bccd6c649264
MaolanaF <95175354+MaolanaF@users.noreply.github.com>
2022-03-13 23:51:20 +0700

    Create NarutoAnime.java

---
+++ Tugas minggu 5/ClassDesignHint/NarutoAnime.java
@@ -0,0 +1,14 @@
+package classhint;
+
+/**
+ *
+ * @author Maolana
+ */
+public class NarutoAnime {
+    public static void main(String[] args) {
+    Sasuke s = new Sasuke();
+    s.printDojutsu();
+    Itachi i = new Sasuke();
+    i.printKekkeiGenkai();
+    }
+}
