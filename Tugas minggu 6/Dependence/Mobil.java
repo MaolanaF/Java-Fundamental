2c146059fcb87cc4f90b994f55a249e6d9fe7c41
MaolanaF <95175354+MaolanaF@users.noreply.github.com>
2022-03-20 22:27:51 +0700

    Create Mobil.java

---
+++ Tugas minggu 6/Dependence/Mobil.java
@@ -0,0 +1,12 @@
+package dependence1;
+
+/**
+ *
+ * @author Maolana
+ */
+public class Mobil implements jenisBensin{
+    @Override
+    public void pakaiBensin(){
+        System.out.println("Mobil menggunakan jenis Pertamax");
+    }
+}
