fc837e910cb715ccf6d15f7d23668ddfbb45c34d
MaolanaF <95175354+MaolanaF@users.noreply.github.com>
2022-03-20 22:27:29 +0700

    Create Motor.java

---
+++ Tugas minggu 6/Dependence/Motor.java
@@ -0,0 +1,12 @@
+package dependence1;
+
+/**
+ *
+ * @author Maolana
+ */
+public class Motor implements jenisBensin{
+    @Override
+    public void pakaiBensin(){
+        System.out.println("Motor menggunakan jenis Pertalite");
+    }
+}
