b6efff85266f3066ef5faf0f50f11a952f0cd6d4
MaolanaF <95175354+MaolanaF@users.noreply.github.com>
2022-03-20 22:21:23 +0700

    Create Pembalap.java

---
+++ Tugas minggu 6/Agregation/Pembalap.java
@@ -0,0 +1,16 @@
+package agregation;
+/**
+ *
+ * @author Maolana
+ */
+public class Pembalap {
+    // Attributes
+    private String nama;
+    private String No;
+
+    // Constructor
+    public Pembalap(String nama, String No) {
+        this.nama = nama;
+        this.No = No;
+    }
+}
