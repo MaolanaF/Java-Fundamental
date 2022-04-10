e632cb818b203d100d095f6e14f36a823af6a66a
MaolanaF <95175354+MaolanaF@users.noreply.github.com>
2022-04-10 23:03:35 +0700

    Create Cat.java

---
+++ Tugas minggu 9/Task 7.1/Cat.java
@@ -0,0 +1,14 @@
+package w9.DynamicPolymorphism;
+
+public class Cat extends Animal {
+    
+    @Override
+    public void sound(){
+        System.out.println("Meow");
+    }
+    
+    public static void main(String args[]) {
+        Animal obj = new Cat();
+        obj.sound();
+    }
+}
